c780cf2740b271f0489746d712459835fe85ca85
Michael Vaden <michaelvaden03@gmail.com>
2022-08-22 17:12:19 -0400

    updated item view into class-based view and added get, post, patch, and delete and implemented get function

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import ItemSerializer
+import json
+
+from django.http import JsonResponse
 from .models import Item
 
 
-class ItemView(viewsets.ModelViewSet):
-    serializer_class = ItemSerializer
-    queryset = Item.objects.all()
+def items(request):
+    """
+    Returns a serialized json response of all items in the database.
+    """
+
+    data = {
+        'data': []
+    }
+
+    for _item in Item.objects.all():
+        _item_data = {
+            'name': _item.name,
+            'for_sale': _item.for_sale,
+            'price': _item.price,
+            'parts': []
+        }
+
+        for _part in _item.parts:
+            _part_data = {
+                'product': _part.product.name,
+                'part': _part.part.name,
+                'amount': _part.amount
+            }
+
+            _item_data['parts'].append(_part_data)
+
+        data['data'].append(_item_data)
+
+    return JsonResponse(json.loads(data))
