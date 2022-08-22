491883253df1ca433fa4a9e267804b306d8f7c2e
Michael Vaden <michaelvaden03@gmail.com>
2022-08-21 23:51:00 -0400

    created 'item' and 'itemrelationrecord' models in core in backend

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,1 +1,26 @@
+from decimal import Decimal
+
 from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    parts = models.ManyToManyField('self', through='ItemRelationRecord')
+    for_sale = models.BooleanField(default=True)
+    price = models.DecimalField(
+        max_digits=8, 
+        decimal_places=2, 
+        default=Decimal('000000.00')
+    )
+
+    class Meta:
+        verbose_name = 'item'
+        verbose_name_plural = 'items'
+
+    def __str__(self) -> str:
+        return self.name 
+
+
+class ItemRelationRecord(models.Model):
+    product = models.ForeignKey(Item, on_delete=models.PROTECT)
+    part = models.ForeignKey(Item, on_delete=models.PROTECT)
